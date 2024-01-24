427197837a5a4e12682f050a277ed0d34038225b
trackmystories <ashirazee@gmail.com>
2024-01-24 18:44:19 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'the-movie-db',
+  base: '/the-movie-db/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
